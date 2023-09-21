1ce5566e4a7c34c60129f39fd032d4e9afc422eb
Leottaro <leo.hafdane@gmail.com>
2023-09-21 09:50:26 +0200

    fix not exact grid

--- src/App.java
+++ src/App.java
@@ -2,12 +2,15 @@ import java.awt.Toolkit;
 import javax.swing.JFrame;
 
 public class App {
+    public static final int tileNumber = 15;
+    public static final int timerDelay = 100;
+    public static final double snakeWidth = 1. / 4;
+
     public static void main(String[] args) throws Exception {
         int boardHeight = (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.75);
+        boardHeight -= boardHeight % tileNumber;
         int boardWidth = boardHeight;
-        int tileSize = boardHeight / 15;
-        int timerDelay = 100;
-        double snakeWidth = 1. / 4;
+        int tileSize = boardHeight / tileNumber;
 
         JFrame frame = new JFrame("Snake");
         frame.setVisible(true);
